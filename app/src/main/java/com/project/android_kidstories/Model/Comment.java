b50f34aaed1c3bfa933f601d0d590afb9470414b
ukejee <ukejee3@gmail.com>
2019-10-10 14:43:49 +0100

    Completed the retrofit client class and add some new response and data models

---
+++ app/src/main/java/com/project/android_kidstories/Model/Comment.java
@@ -0,0 +1,90 @@
+package com.project.android_kidstories.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * @author .: Ukeje Emeka
+ * @email ..: ukejee3@gmail.com
+ * @created : 10/10/19
+ */
+public class Comment {
+
+    @SerializedName("id")
+    @Expose
+    private Integer id;
+    @SerializedName("story_id")
+    @Expose
+    private Integer storyId;
+    @SerializedName("user_id")
+    @Expose
+    private Integer userId;
+    @SerializedName("body")
+    @Expose
+    private String body;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+    @SerializedName("user")
+    @Expose
+    private User user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getStoryId() {
+        return storyId;
+    }
+
+    public void setStoryId(Integer storyId) {
+        this.storyId = storyId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}

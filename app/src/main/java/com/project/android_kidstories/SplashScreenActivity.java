a30b55975c47e6c263bbe57efc20b3f87ea2dae4
Noel-elon <noelnwaelugo@gmail.com>
2019-10-10 23:55:27 +0100

    Minor changes

--- app/src/main/java/com/project/android_kidstories/SplashScreenActivity.java
+++ app/src/main/java/com/project/android_kidstories/SplashScreenActivity.java
@@ -16,7 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), OnBoardingActivity.class);
                 startActivity(intent);
                 finish();
             }

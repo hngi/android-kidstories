9ef78af80b0fb9d7cfb4921d19122f288497b466
Adesire <desilere@gmail.com>
2019-10-11 00:27:32 +0100

    Convert NewStories, PopularStories and Categories to fragmenta and linking them

---
+++ app/src/main/java/com/project/android_kidstories/fragments/CategoriesFragment.java
@@ -0,0 +1,33 @@
+package com.project.android_kidstories.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.project.android_kidstories.R;
+
+import butterknife.ButterKnife;
+
+public class CategoriesFragment extends Fragment {
+
+    public static CategoriesFragment newInstance(){return new CategoriesFragment();}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_categories,container,false);
+        ButterKnife.bind(this,v);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+    }
+
+}

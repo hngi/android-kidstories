96b8c472b26a41159e3eab3fbf279e11f92974ac
segunfrancis <francis4dblues@gmail.com>
2019-10-11 00:28:11 +0100

    add search menu resource

--- app/src/main/java/com/project/android_kidstories/OnBoardingActivity.java
+++ app/src/main/java/com/project/android_kidstories/OnBoardingActivity.java
@@ -2,7 +2,9 @@ package com.project.android_kidstories;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
@@ -10,5 +12,41 @@ public class OnBoardingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+    }
+
+    public void openDonateActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, DonateActivity.class));
+    }
+
+    public void openAboutActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, StoryListingActivity.class));
+    }
+
+    public void openLoginActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, LoginActivity.class));
+    }
+
+    public void openRegisterActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, RegisterActivity.class));
+    }
+
+    public void openMainActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, MainActivity.class));
+    }
+
+    public void openBookDetailActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, StoryDetailActivity.class));
+    }
+
+    public void openAddStoryActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, AddStoryActivity.class));
+    }
+
+    public void openEditProfileActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, ProfileActivity.class));
+    }
+
+    public void openBookmarkActivity(View view) {
+        startActivity(new Intent(OnBoardingActivity.this, BookMark.class));
     }
 }

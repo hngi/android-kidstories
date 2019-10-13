b035cf9fb5a4471e395ca865c1b256242d8805ad
segunfrancis <francis4dblues@gmail.com>
2019-10-13 12:31:07 +0100

    implementation: Facebook Authentication - facebook logout

--- app/src/main/java/com/project/android_kidstories/ui/auth/LogOutFragment.java
+++ app/src/main/java/com/project/android_kidstories/ui/auth/LogOutFragment.java
@@ -9,12 +9,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.facebook.login.LoginManager;
+
 public class LogOutFragment extends Fragment {
 
     private LogOutViewModel logOutViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         logOutViewModel = ViewModelProviders.of(this).get(LogOutViewModel.class);
+        // Logout of facebook
+        if (LoginManager.getInstance() != null) {
+            LoginManager.getInstance().logOut();
+        }
         getActivity().finish();
         return null;
     }

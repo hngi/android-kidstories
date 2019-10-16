7937490ba68bcb39e0140a7dde10fa8ab6f4d7df
Oluwajuwon-F <fawoleoluwajuwon@gmail.com>
2019-10-16 17:10:40 +0100

    New Stories

--- app/src/main/java/com/project/android_kidstories/Views/main/ui/home/Fragments/NewStoriesFragment.java
+++ app/src/main/java/com/project/android_kidstories/Views/main/ui/home/Fragments/NewStoriesFragment.java
@@ -1,10 +1,12 @@
-package com.project.android_kidstories.ui.home.Fragments;
+package com.project.android_kidstories.Views.main.ui.home.Fragments;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,13 +15,16 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.project.android_kidstories.Api.Api;
 import com.project.android_kidstories.Api.Responses.story.StoryAllResponse;
+import com.project.android_kidstories.Api.RetrofitClient;
 import com.project.android_kidstories.DataStore.ApiViewmodel;
 import com.project.android_kidstories.DataStore.Repository;
 import com.project.android_kidstories.Model.Story;
 import com.project.android_kidstories.R;
 import com.project.android_kidstories.Utils.Common;
 import com.project.android_kidstories.Views.main.ui.home.BaseFragment;
+import com.project.android_kidstories.adapters.RecyclerStoriesAdapter;
 import com.project.android_kidstories.ui.home.StoryAdapter;
 
 import java.util.List;
@@ -32,7 +37,8 @@ public class NewStoriesFragment extends BaseFragment implements StoryAdapter.OnS
 
     private static final String TAG = "kidstories";
     private RecyclerView recyclerView;
-    ;
+    private RecyclerStoriesAdapter adapter;
+    ProgressDialog progressDoalog;
     private Repository repository;
     private StoryAdapter storyAdapter;
 
@@ -40,7 +46,6 @@ public class NewStoriesFragment extends BaseFragment implements StoryAdapter.OnS
     public static NewStoriesFragment newInstance() {
         return new NewStoriesFragment();
     }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -49,6 +54,7 @@ public class NewStoriesFragment extends BaseFragment implements StoryAdapter.OnS
         FloatingActionButton fab = v.findViewById(R.id.new_story_frag_fab);
         fab.setOnClickListener(this);
 
+        /*
         recyclerView = v.findViewById(R.id.new_story_frag_recycler);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
@@ -60,7 +66,40 @@ public class NewStoriesFragment extends BaseFragment implements StoryAdapter.OnS
         recyclerView.setAdapter(storyAdapter);
 
 
-        fetchStories();
+        fetchStories();*/
+        progressDoalog = new ProgressDialog(getActivity());
+        progressDoalog.setMessage("Loading....");
+        progressDoalog.show();
+
+        /*Create handle for the RetrofitInstance interface*/
+        Api service = RetrofitClient.getInstance().create(Api.class);
+        Call<StoryAllResponse> stories = service.getAllStories();
+        Log.i("apple", "Size: "+stories.isExecuted());
+
+        stories.enqueue(new Callback<StoryAllResponse>() {
+            @Override
+            public void onResponse(Call<StoryAllResponse> call, Response<StoryAllResponse> response) {
+                //  generateCategoryList(response.body(),v);
+                progressDoalog.dismiss();
+                recyclerView = v.findViewById(R.id.recyclerView);
+
+                try {
+                    adapter = new RecyclerStoriesAdapter(getContext(), response.body());
+                    GridLayoutManager layoutManager = new GridLayoutManager(getContext(), 1);
+                    recyclerView.setLayoutManager(layoutManager);
+                    recyclerView.setAdapter(adapter);
+                }catch (Exception e){
+                    Toast.makeText(getContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<StoryAllResponse> call, Throwable t) {
+                progressDoalog.dismiss();
+
+                Toast.makeText(getContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return v;
     }

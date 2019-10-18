4ca187edab497ce29a04ea00e53f2edb1780b80b
Vague <vaguemail369@gmail.com>
2019-10-18 06:21:42 +0100

    If user is already logged in, go to MainActivity

--- app/src/main/java/com/project/android_kidstories/SplashScreenActivity.java
+++ app/src/main/java/com/project/android_kidstories/SplashScreenActivity.java
@@ -3,7 +3,10 @@ package com.project.android_kidstories;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import androidx.appcompat.app.AppCompatActivity;
+import com.project.android_kidstories.Views.main.MainActivity;
+import com.project.android_kidstories.sharePref.SharePref;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -15,10 +18,18 @@ public class SplashScreenActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                SharePref sharePref = SharePref.getINSTANCE(getApplicationContext());
+                String userToken = sharePref.getString("Token");
+                if (TextUtils.isEmpty(userToken)) {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
 
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
             }
             //the delay time is 3s
         }, 3000);

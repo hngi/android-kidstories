fe0c30334c8a55526c62378204d44ef42e7f99f6
joshbillionz <joshuaerondu4@gmail.com>
2019-10-31 07:43:30 +0100

    SQLite codes for offline storage

--- app/src/main/java/com/project/android_kidstories/database/StoryBaseHelper.java
+++ app/src/main/java/com/project/android_kidstories/database/StoryBaseHelper.java
@@ -1,22 +1,39 @@
 package com.project.android_kidstories.database;
 
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.project.android_kidstories.database.StoryDbSchema.StoryTable;
+
 import androidx.annotation.Nullable;
 
 public class StoryBaseHelper extends SQLiteOpenHelper {
+    private static final int VERSION= 1;
+    private static final String DATABASE_NAME = "storyBase.Db";
 
-    public StoryBaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    public StoryBaseHelper(Context context){
+        super(context,DATABASE_NAME,null,VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + StoryDbSchema.StoryTable.NAME + "(" +
+                " _id integer primary key autoincrement, " +
+                StoryTable.Cols.ID + ", " +
+                StoryTable.Cols.TITLE + ", " +
+                StoryTable.Cols.AUTHOR + ", " +
+                /* StoryTable.Cols.CATEGORY + ", " +*/
+                StoryTable.Cols.BODY + ", " +
+                StoryTable.Cols.AGE + ", " +
+                StoryTable.Cols.IMAGE +
+                ")"
+        );
+
 
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
@@ -24,14 +41,3 @@ public class StoryBaseHelper extends SQLiteOpenHelper {
 }
 
 
-//{
-//}
-//    spackage com.bignerdranch.android.criminalintent.database;
-//
-//        import android.content.Context;
-//        import android.database.sqlite.SQLiteDatabase;
-//        import android.database.sqlite.SQLiteOpenHelper;
-//
-//        import com.bignerdranch.android.criminalintent.database.CrimeDbSchema.CrimeTable;
-//
-//public class CrimeBaseHelper extends

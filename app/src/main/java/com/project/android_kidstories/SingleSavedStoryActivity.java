64e82d5e195bd04db45d565f11c9c01a1174e783
joshbillionz <joshuaerondu4@gmail.com>
2019-11-02 12:25:03 +0100

    little more work on save stories

--- app/src/main/java/com/project/android_kidstories/SingleSavedStoryActivity.java
+++ app/src/main/java/com/project/android_kidstories/SingleSavedStoryActivity.java
@@ -1,8 +1,12 @@
 package com.project.android_kidstories;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,21 +19,47 @@ public class SingleSavedStoryActivity extends AppCompatActivity {
     Story story ;
     StoryLab storyLab;
     TextView authorTv, storyContentTv;
+    private Toolbar toolbar;
+
+    private ImageButton ZoomIn, ZoomOut;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_saved_story);
-        int storyId = getIntent().getIntExtra(SavedStoriesAdapter.KEY_STORY_ID,-1);
+        toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
         String storyTitle = getIntent().getStringExtra(SavedStoriesAdapter.KEY_STORY_TITLE);
 
-        storyLab= StoryLab.get(this);
 
-        authorTv = findViewById(R.id.saved_story_content);
+
+        storyLab= StoryLab.get(this);
+        story = storyLab.getStory(storyTitle);
+
+        authorTv = findViewById(R.id.saved_story_author);
         storyContentTv = findViewById(R.id.saved_story_content);
 
-        story = storyLab.getStory(storyTitle);
+        ZoomIn = findViewById(R.id.Zoom_In);
+        ZoomOut = findViewById(R.id.Zoom_Out);
+        ZoomIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storyContentTv.getTextSize();
+                storyContentTv.setTextSize(24);
+                storyContentTv.setMovementMethod(new ScrollingMovementMethod());
+            }
+        });
+        ZoomOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                storyContentTv.getTextSize();
+                storyContentTv.setTextSize(14);
+                storyContentTv.setMovementMethod(new ScrollingMovementMethod());
+            }
+        });
+
 
         if (story==null){
 
@@ -39,15 +69,18 @@ public class SingleSavedStoryActivity extends AppCompatActivity {
 
         }
         else{
-            if(!storyTitle.equals(null)){
-
+            if(storyTitle!=null){
+                getSupportActionBar().setTitle(storyTitle);
                 showStory(storyLab.getStory(storyTitle));
-
             }
         }
     }
 
     public void showStory(Story story){
+        authorTv.setText(story.getAuthor());
         storyContentTv.setText(story.getBody());
+
     }
+
+
 }

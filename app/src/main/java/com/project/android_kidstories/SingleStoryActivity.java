ab1905ff0b8dd17965d9a65f67a62213c94cdcd3
joshbillionz <joshuaerondu4@gmail.com>
2019-11-02 17:07:44 +0100

    fixed logout error cause by CommentActivity back key press

--- app/src/main/java/com/project/android_kidstories/SingleStoryActivity.java
+++ app/src/main/java/com/project/android_kidstories/SingleStoryActivity.java
@@ -387,23 +387,4 @@ public class SingleStoryActivity extends AppCompatActivity {
         } finally {
                    }
     }
-    public static Bitmap loadBitmap(Context context, String picName){
-        Bitmap b = null;
-        FileInputStream fis;
-        try {
-            fis = context.openFileInput(picName);
-            b = BitmapFactory.decodeStream(fis);
-        }
-        catch (FileNotFoundException e) {
-            Log.d("tag", "file not found");
-            e.printStackTrace();
-        }
-        catch (IOException e) {
-            Log.d("tag", "io exception");
-            e.printStackTrace();
-        } finally {
-
-        }
-        return b;
-    }
 }

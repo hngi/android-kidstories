0e08d9ff896552318b69e8d984203921592bd02a
Vague <vaguemail369@gmail.com>
2019-11-05 23:25:29 +0100

    Setup recyclerviews with adapters in Home Fragment

--- app/src/main/java/com/project/android_kidstories/ui/home/HomeFragment.java
+++ app/src/main/java/com/project/android_kidstories/ui/home/HomeFragment.java
@@ -3,10 +3,23 @@ package com.project.android_kidstories.ui.home;
 import android.os.Bundle;
 import android.view.*;
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import com.project.android_kidstories.R;
+import com.project.android_kidstories.data.model.Story;
 import com.project.android_kidstories.ui.base.BaseFragment;
+import com.project.android_kidstories.ui.home.adapters.ExploreAdapter;
+import com.project.android_kidstories.ui.home.adapters.PopularStoriesAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends BaseFragment {
+
+    private List<Story> stories = new ArrayList<>();
+    private List<Story> populars = new ArrayList<>();
+
+    private ExploreAdapter exploreAdapter;
+    private PopularStoriesAdapter popularStoriesAdapter;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -14,7 +27,23 @@ public class HomeFragment extends BaseFragment {
         // This fragment needs to show a menu
         setHasOptionsMenu(true);
 
+        // Setup the recyclerviews
+        RecyclerView recyclerViewExplore = root.findViewById(R.id.recyclerview_explore_stories);
+        RecyclerView recyclerViewPopularStories = root.findViewById(R.id.recyclerview_popular_stories);
+
+        exploreAdapter = new ExploreAdapter(stories);
+        popularStoriesAdapter = new PopularStoriesAdapter(populars);
+
+        recyclerViewExplore.setAdapter(exploreAdapter);
+        recyclerViewPopularStories.setAdapter(popularStoriesAdapter);
+
+        updateData();
+
         return root;
+    }
+
+    private void updateData() {
+
     }
 
     @Override

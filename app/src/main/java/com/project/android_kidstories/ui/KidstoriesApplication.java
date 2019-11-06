5c70fda6eed908a07b9c601521fc5de166e3dace
Vague <vaguemail369@gmail.com>
2019-11-06 11:44:49 +0100

    Clean up application

--- app/src/main/java/com/project/android_kidstories/ui/KidstoriesApplication.java
+++ app/src/main/java/com/project/android_kidstories/ui/KidstoriesApplication.java
@@ -1,19 +1,9 @@
 package com.project.android_kidstories.ui;
 
 import android.app.Application;
-import android.content.Context;
-import android.content.ContextWrapper;
-import android.content.SharedPreferences;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatDelegate;
-import com.google.gson.Gson;
-import com.pixplicity.easyprefs.library.Prefs;
-import com.project.android_kidstories.data.source.helpers.AddStoryHelper;
-
-import java.util.Map;
+import com.project.android_kidstories.data.source.local.preferences.SharePref;
 
 public class KidstoriesApplication extends Application {
     private static final String TAG = "Common";
@@ -24,66 +14,11 @@ public class KidstoriesApplication extends Application {
         super.onCreate();
         Log.d(TAG, "onCreate: Common");
 
-        new Prefs.Builder()
-                .setContext(this)
-                .setMode(ContextWrapper.MODE_PRIVATE)
-                .setPrefsName(getPackageName())
-                .setUseDefaultSharedPreference(true)
-                .build();
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        Map<String, ?> map1;
-        Gson gson = new Gson();
-        map1 = prefs.getAll();
-        Log.d(TAG, "map = " + map1);
-
-        if (prefs.contains("NIGHT MODE")) {
-            Boolean night1 = (Boolean) map1.get("NIGHT MODE");
-
-            if (night1) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            } else
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-        } else
-            prefs.edit().putBoolean("NIGHT MODE", false).apply();
-
-
-        prefs.registerOnSharedPreferenceChangeListener((sharedPreferences, key) -> {
-            Log.d(TAG, "key" + key);
-            Map<String, ?> map;
-            map = sharedPreferences.getAll();
-            Log.d(TAG, "map = " + map);
-            String night = String.valueOf(map.get("NIGHT MODE"));
-
-
-            if (night == "true") {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                Log.d(TAG, "Night mode is changed to set");
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                Log.d(TAG, "Night mode is unset");
-
-            }
-
-        });
+        SharePref sharePref = SharePref.getInstance(this);
+        if (sharePref.getNightMode()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
     }
-
-    public static boolean checkNetwork(Context context) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-    }
-
-    private void saveToken(String token) {
-        Prefs.putString(AddStoryHelper.TOKEN_KEY, token);
-    }
-
-    private String getSavedToken(String token) {
-        return Prefs.getString(AddStoryHelper.TOKEN_KEY, "aTokenStringShouldBeHere");
-    }
-
-    public static void updateSharedPref(int storyId,boolean value){
-        Prefs.putBoolean(String.valueOf(storyId),value);
-    }
-
 }

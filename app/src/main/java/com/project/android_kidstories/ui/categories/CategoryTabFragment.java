195d1efa0dbf755476b83e21edd1e54a6b6a869f
Vague <vaguemail369@gmail.com>
2019-11-06 09:46:28 +0100

    Create the category tab layout

--- app/src/main/java/com/project/android_kidstories/ui/categories/CategoryTabFragment.java
+++ app/src/main/java/com/project/android_kidstories/ui/categories/CategoryTabFragment.java
@@ -7,12 +7,36 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import com.project.android_kidstories.R;
+import com.project.android_kidstories.ui.home.adapters.ExploreAdapter;
 
 public class CategoryTabFragment extends Fragment {
+
+    private String category_name;
+
+    private ExploreAdapter adapter;
+
+    public CategoryTabFragment(String category_name) {
+        this.category_name = category_name;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View root = inflater.inflate(R.layout.fragment_category_tab, container, false);
+
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerview_category_tab);
+        adapter = new ExploreAdapter(requireContext());
+
+        recyclerView.setAdapter(adapter);
+
+        getCategoryStories();
+
+        return root;
+    }
+
+    private void getCategoryStories() {
+
     }
 }

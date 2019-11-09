e8c81e482c7a5a7e407a92ae48f225201d052645
Vague <vaguemail369@gmail.com>
2019-11-09 14:34:14 +0100

    Use the pico package for tts

--- app/src/main/java/com/project/android_kidstories/ui/story_viewing/SingleStoryActivity.java
+++ app/src/main/java/com/project/android_kidstories/ui/story_viewing/SingleStoryActivity.java
@@ -329,7 +329,7 @@ public class SingleStoryActivity extends BaseActivity {
                 }
             }
 
-        }, googleTtsPackage);
+        }, picoPackage);
 
         //background Music and tts
         backgroundMusicPlayer = MediaPlayer.create(this, R.raw.kidsong2);
